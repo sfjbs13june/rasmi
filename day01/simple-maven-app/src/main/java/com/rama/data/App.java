f7326d99a7726ef25b8ad2155ebf15e3a3187d33
admin <rashmidevalanka@gmail.com>
2023-09-10 22:59:19 -0700

    cloned from rama repo

---
+++ day01/simple-maven-app/src/main/java/com/rama/data/App.java
@@ -0,0 +1,11 @@
+package com.rama.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, mohanashankari here");
+    }
+}
