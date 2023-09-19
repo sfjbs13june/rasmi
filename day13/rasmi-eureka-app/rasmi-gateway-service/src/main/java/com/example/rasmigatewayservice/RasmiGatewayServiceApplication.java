ac42d8fd760d2633f6a88404084300ee9e71577c
admin <rashmidevalanka@gmail.com>
2023-09-19 01:21:31 -0700

    Day13: Hospital management Eureka app

---
+++ day13/rasmi-eureka-app/rasmi-gateway-service/src/main/java/com/example/rasmigatewayservice/RasmiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rasmigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasmiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasmiGatewayServiceApplication.class, args);
+	}
+
+}
