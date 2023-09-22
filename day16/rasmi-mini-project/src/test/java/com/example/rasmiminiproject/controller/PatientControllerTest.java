22765dafc2c37d66b5e361868a0cc9d96b5d6fdb
admin <rashmidevalanka@gmail.com>
2023-09-22 03:06:50 -0700

    Mini project- Hospital Management System

--- day16/rasmi-mini-project/src/test/java/com/example/rasmiminiproject/controller/PatientControllerTest.java
+++ day16/rasmi-mini-project/src/test/java/com/example/rasmiminiproject/controller/PatientControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class PatientControllerTest {
     @InjectMocks
    private PatientController patientController;
@@ -14,13 +17,21 @@ public class PatientControllerTest {
     @Mock
     private AppointmentRepository appointmentRepotest;
 
+    @Mock
+    private Appointment appointment;
+
     @Test
     public void getMyAppointmentsTest()
     {
-        Prescription prescription=new Prescription("pre1","app1","fever","pat1","doctor1");
-
-        Appointment appointment= new Appointment("app1","pat1","doctor1","21/09/2023",prescription);
-
+        Appointment appointment=new Appointment();
+        appointment.setAppointmentId("app1");
+        appointment.setDoctorName("doctor1");
+        appointment.setDate("22/09/2023");
+        when(appointmentRepotest.save(appointment)).thenReturn(appointment);
+        Appointment result=patientController.saveAppointment(appointment);
+        assertEquals("id1",result.getAppointmentId());
+        assertEquals("app1",result.getDoctorName());
+        assertEquals("22/09/2023",result.getDate());
 
     }
 
